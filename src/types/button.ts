c5e33d0265dd2ad21e089e4bdbd666c900cfdf05
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2025-06-28 14:39:43 -0300

    refactor(button): :recycle: ajustar tipagem de ButtonProps para utilizar buttonVariants
    - Modificada a interface ButtonProps para que utilize a tipagem correta de buttonVariants, melhorando a segurança e a previsibilidade do código.

--- src/types/button.ts
+++ src/types/button.ts
@@ -1,5 +1,6 @@
 import { VariantProps } from "class-variance-authority";
+import { buttonVariants } from "@/components/ui/button";
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<any> {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 } 
